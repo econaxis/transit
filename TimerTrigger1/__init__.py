7e89dda4bf213689f73ee72d62f328ffa69f6f41
Henry Nguyen <henry2833@gmail.com>
2021-07-08 12:22:23 -0700

    commit

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,7 +1,11 @@
 import azure.functions as func
 import requests
-import my_pb2
+try:
+    import my_pb2
+except ImportError:
+    from . import my_pb2
 import pyodbc
+import logging
 
 sqlconn = pyodbc.connect(
     "Driver={ODBC Driver 17 for SQL Server};Server=tcp:translink-transit.database.windows.net,1433;Database=transit;Uid=martinliu24;Pwd=LakxXp46LHCvTTL$;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
@@ -10,6 +14,7 @@ cursor.fast_executemany = True
 
 
 def main(mytimer: func.TimerRequest) -> None:
+    logging.info("Starting")
     response = requests.get("https://gtfs.translink.ca/v2/gtfsposition?apikey=XkJgz46eM82zRr0B3GD7").content
     msg = my_pb2.FeedMessage()
     msg.ParseFromString(response)
@@ -20,9 +25,6 @@ def main(mytimer: func.TimerRequest) -> None:
         to_insert.append((vehicle.trip.trip_id, vehicle.trip.start_date, vehicle.trip.route_id, vehicle.trip.direction_id,
               vehicle.position.latitude, vehicle.position.longitude,
               vehicle.current_stop_sequence, vehicle.timestamp, vehicle.stop_id, vehicle.vehicle.id))
-    print("inserting")
+    logging.info("inserting")
     cursor.executemany("INSERT INTO realtime VALUES (?,?,?,?,?,?,?,?,?,?)", to_insert)
     sqlconn.commit()
-
-if __name__ == '__main__':
-    main(None)
