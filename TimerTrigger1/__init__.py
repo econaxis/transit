4a2de99f4f45384219f078829db70dee782d3df9
Henry Nguyen <henry2833@gmail.com>
2021-07-07 22:28:18 -0700

    changed to redis

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -2,16 +2,13 @@ import azure.functions as func
 import logging
 import azure
 from azure.storage.blob import BlobServiceClient, generate_account_sas, ResourceTypes, AccountSasPermissions
-from azure.storage.queue import QueueClient
 from datetime import date, datetime
 import requests
 import zstandard as zstd
+import redis
 
+redis = redis.StrictRedis (host="redis-18070.c53.west-us.azure.cloud.redislabs.com", port = 18070, password="nhWkkCADbwbFrLG9dVjFzPOqCWcQJ6LZ")
 blob_service_client = BlobServiceClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=storageaccounttransbc67;AccountKey=De187+KqHglpnjS3Uj+48Xp2cW4uSZU8GXZ5LDAPW5eqI3kpMyUXSbbXUmZv1rQsdTAdgb1QdRDdm9VsaixEww==;EndpointSuffix=core.windows.net")
-queue_client = QueueClient.from_connection_string(
-    conn_str="DefaultEndpointsProtocol=https;AccountName=storageaccounttransbc67;AccountKey=De187+KqHglpnjS3Uj+48Xp2cW4uSZU8GXZ5LDAPW5eqI3kpMyUXSbbXUmZv1rQsdTAdgb1QdRDdm9VsaixEww==;EndpointSuffix=core.windows.net"
-    , queue_name="transit-queue")
-
 compressor = None
 
 
@@ -30,4 +27,4 @@ def main(mytimer: func.TimerRequest, context: azure.functions.Context) -> None:
     response = compressor.compress(response)
     logging.info(len(response))
     blob_client.upload_blob(response, length = len(response), metadata = {"zstd": "true"})
-    queue_client.send_message(date)
+    redis.lpush("queue", date)
